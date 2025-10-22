ca966bcb91e214902b23485c96ba1cf80cde455d
anuj808 <anujrajbhar809@gmail.com>
2025-10-22 19:30:58 +0530

    Updated project files

--- server/server.js
+++ server/server.js
@@ -3,27 +3,34 @@ import cors from "cors";
 import 'dotenv/config';
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
-import authRouter from "./routes/authroute.js"
+import authRouter from "./routes/authroute.js";
 import userRouter from "./routes/userRoute.js";
-const app = express();
 
+const app = express();
 const port = process.env.PORT || 4000;
+
+// Connect to database
 connectDB();
 
-// const allowedOrigins=['https://authsystem-frontend.onrender.com/']
+// ✅ Updated frontend URL
+app.use(cors({
+  origin: ["https://authsystemfrontend-wt73.onrender.com"],
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({origin:["https://authsystem-frontend.onrender.com"], credentials: true }));
+// Test route
+app.get("/", (req, res) => {
+  res.send("API is Working !");
+});
 
-app.get("/" , (req , res)=>{
-   res.send("API is Working ! "); 
-})
-app.use('/api/auth', authRouter);
-app.use('/api/user', userRouter);
+// Routes
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
-
+// Server
 app.listen(port, () => {
   console.log(`Server started on Port: ${port}`);
 });
